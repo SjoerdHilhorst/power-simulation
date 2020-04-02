29c2f4266c6c673d1cc91add01a9f6fe7433fcc3
m-ariya <hsmariya@gmail.com>
2020-04-02 12:15:22 +0200

    small correction to 'simulation' regarding json configuration

--- gps/simulation.py
+++ gps/simulation.py
@@ -6,12 +6,13 @@ from powers import *
 
 # Uncomment the option you want to test
 
+configuration = None
 #configuration = "CUSTOM"
-filename = "custom"
-configuration = "DEFAULT"  # default configuration; floats are stored in "SCALE"
+
+filename = "custom"  # it can be any name provided by user, new JSON will be created
 
 # if it is custom user provides information and config is modified, here it is hardcoded for now
-if configuration == "CUSTOM":
+if configuration is not None:
     float_mode = "SCALE"  # uncomment if user wants to store float using scaling and one register
     # float_mode = "COMB"  # uncomment if user wants to store float in IEEE  754 format
     endian = Endian.Big
@@ -21,12 +22,13 @@ if configuration == "CUSTOM":
     fx_addr_separator = 100
     # makes a new json with these variables at "Config/filename.json"
     json_config.set_custom_json(filename, fx_addr_separator, float_mode, endian, endian, scaling_factor, addresses)
+    configuration = filename
 
 address = ("localhost", 5030)
 id = "GREENER_001"
 
 # battery is instantiated, only constants are in the input, "custom" or None is for accessing the json
-battery = Battery(id, address, None if configuration == "DEFAULT" else "custom", 1, 5, 1, 1)
+battery = Battery(id, address, configuration, 1, 5, 1, 1)
 
 power_source = PowerIn((0, 200), (-65, 185))
 power_load = PowerOut((-11, 25), (-15, 25))
