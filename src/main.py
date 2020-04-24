60c35d66247f8a7e2542848a11a53a3311b9c3c6
SjoerdHilhorst <48722300+SjoerdHilhorst@users.noreply.github.com>
2020-04-24 21:48:21 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -35,9 +35,7 @@ if __name__ == "__main__":
         raise LookupError("This simulation type does not exist: ", sim_type)
 
     battery.run_server()
-
-    
-    
+  
     if env["graph"]["enabled"]:
         from graph import Graph
         graph_env = env["graph"]
@@ -48,10 +46,4 @@ if __name__ == "__main__":
         graph.run()
     else:
         power_sim.run_simulation()
-
-
-
-
-
-
-
+        
