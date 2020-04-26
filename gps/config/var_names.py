091f3b7834dcba226c2b3c11e131efc1334cc534
m-ariya <hsmariya@gmail.com>
2020-04-26 23:06:30 +0200

    trying to fix packages and include address changes

--- gps/config/var_names.py
+++
@@ -1,7 +0,0 @@
-comb = "COMB"
-scale = "SCALE"
-
-coil = 1
-discrete = 2
-holding = 3
-input = 4
--- gps/simulations/__init__.py
+++
@@ -1,5 +0,0 @@
-from .historic_simulation import HistoricSimulation
-from .random_simulation import RandomSimulation
-from .simulation_model import Simulation
-
-
