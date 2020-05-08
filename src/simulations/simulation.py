9ef41ab97ce9cb8a47f589555ff356ef3e9460aa
m-ariya <hsmariya@gmail.com>
2020-05-08 17:01:07 +0200

    user can define custom fields

--- src/simulations/simulation.py
+++ src/simulations/simulation.py
@@ -1,6 +1,5 @@
 import random
 
-
 from numpy.ma import sin
 
 from csv_reader import CSVReader
@@ -16,7 +15,7 @@ class Simulation(SimulationSuper):
         super().__init__(battery, address)
         self.csv_reader = CSVReader(self.env['from_csv'])
 
-# examples
+    # examples
     def get_active_power_in(self):
         api = self.csv_reader.get_from_csv('active_power_in')
         if not self.battery.is_input_connected():
@@ -30,7 +29,7 @@ class Simulation(SimulationSuper):
         return rpi
 
     def get_active_power_out(self):
-        apo = random.randint(1,3) * sin(self.t + random.randint(0,15)) + random.randint(1,40)
+        apo = random.randint(1, 3) * sin(self.t + random.randint(0, 15)) + random.randint(1, 40)
         return float(apo)
 
     def get_reactive_power_out(self):
@@ -39,3 +38,13 @@ class Simulation(SimulationSuper):
 
     def get_current_I1_in(self):
         return 200
+
+    def get_custom(self):
+        return sin(self.t)
+
+    def get_custom1(self):
+        return 69
+
+    def update_custom(self):
+        self.battery.set_value(self.fields['custom'], self.get_custom())
+        self.battery.set_value(self.fields['custom1'], self.get_custom1())
