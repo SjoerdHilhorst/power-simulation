d62a40294fbce00c05798219e4cb79bd5ea38a90
m-ariya <hsmariya@gmail.com>
2020-06-12 18:39:34 +0200

    setup changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="power_simulation",
     version='0.1',
     package_dir={'': 'src'},
-    packages=['server', 'config'],
+    packages=find_packages(include=['server', 'config', 'simulation', 'output']),
     author='Mariya Shumska, Chris Worthington, Victor Florea, Sjoerd Hilhorst',
     install_requires=[
         'pandas',
