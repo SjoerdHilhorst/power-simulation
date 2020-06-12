b0dd18f09c71bafac4bf7c23531f380c27d36a68
m-ariya <hsmariya@gmail.com>
2020-06-12 21:15:37 +0200

    tests moved to src

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="power_simulation",
     version='0.1',
-    package_dir={'': 'src'},
     packages=find_packages(include=['server', 'config', 'simulation', 'output']),
     author='Mariya Shumska, Chris Worthington, Victor Florea, Sjoerd Hilhorst',
     install_requires=[
